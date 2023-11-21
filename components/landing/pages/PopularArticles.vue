ba58412f99333f8b20f6944913b7874cf50c00c9
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-11-21 23:57:53 +0600

    fix: changes after moving files from APFS to NTFS

--- components/landing/pages/PopularArticles.vue
+++ components/landing/pages/PopularArticles.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="main"></div>
+  <div class="main">
+    <div class="content">
+      <div class="title">Most popular articles</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,5 +20,27 @@ export default Vue.extend({});
   width: 100vw;
 
   background-color: $white;
+}
+
+.content {
+  margin-left: 121px;
+}
+
+.title {
+  width: 737px;
+  height: 76px;
+  padding-top: 189px;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 64px;
+  line-height: 75px;
+
+  /* spec-green-gradient */
+
+  background: $primary_gradient;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
 }
 </style>
